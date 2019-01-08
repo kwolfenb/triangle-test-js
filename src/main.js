0435030143bcce7aa1166aaf80739b59a03ed946
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2019-01-08 00:10:18 -0800

    resolves error

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import { pingPong } from './ping-pong';
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
@@ -8,10 +7,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 $(document).ready(function() {
   $('#ping-pong-form').submit(function(event) {
     event.preventDefault();
-    var goal = $('#goal').val();
-    var output = pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
+
   });
 });
