00cf49001fa5e21d288261299e63771cf1a2cf45
Guest User <Guest@epicodus-100.local>
2019-01-08 08:52:31 -0800

    adds tests to triangle tracker

--- spec/triangle-spec.js
+++ spec/triangle-spec.js
@@ -1,15 +1,27 @@
 import { Triangle } from './../src/triangle.js';
 describe('Triangle', function() {
+  var reusableTriangle;
+
+  beforeEach(function() {
+    reusableTriangle = new Triangle(5,5,5);
+  });
+
+  it('should show how beforeEach works', function() {
+    console.log(reusableTriangle);
+  });
 
   it('should test whether a Triangle has three sides', function() {
-    var triangle = new Triangle(3,4,5);
-    expect(triangle.side1).toEqual(3);
-    expect(triangle.side2).toEqual(4);
-    expect(triangle.side3).not.toEqual(6);
+    expect(reusableTriangle.side1).toEqual(5);
+    expect(reusableTriangle.side2).toEqual(5);
+    expect(reusableTriangle.side3).not.toEqual(6);
   });
 
   it('should correctly determine whether three lengths can be made into a triangle', function() {
     var notTriangle = new Triangle(3,9,22);
     expect(notTriangle.checkType()).toEqual("not a triangle");
+  });
+
+  it('should determine whether a triangle with 3 equal sides is equalateral', function() {
+    expect(reusableTriangle.isEqualateral()).toEqual("this is equalateral");
   });
 });
--- src/triangle.js
+++ src/triangle.js
@@ -4,8 +4,15 @@ export function Triangle(side1, side2, side3) {
   this.side3=side3;
 }
 
+
 Triangle.prototype.checkType = function() {
   if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
     return "not a triangle";
   }
 };
+
+Triangle.prototype.isEqualateral = function() {
+  if((this.side1 == this.side2) && (this.side2 == this.side3)) {
+    return "this is equalateral";
+  }
+}
